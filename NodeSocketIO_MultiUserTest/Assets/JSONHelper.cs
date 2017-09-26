12ea699721e9397a681c44c3b17bb35698810d8c
Anthony Scavarelli <anthony.scavarelli@gmail.com>
2017-09-26 14:54:24 -0400

    have Unity receiving and updating information from node server. Now works with web tests.

---
+++ NodeSocketIO_MultiUserTest/Assets/JSONHelper.cs
@@ -0,0 +1,35 @@
+﻿//modified from https://stackoverflow.com/questions/38390274/unity-c-how-to-convert-an-array-of-a-class-into-json
+using System;
+
+public class JSONHelper
+{
+    public static T FromJsonObject<T>(string json)
+    {
+        T obj = UnityEngine.JsonUtility.FromJson<T>(json);
+        return obj;
+    }
+
+    public static T[] FromJsonArray<T>(string json)
+    {
+        Wrapper<T> wrapper = UnityEngine.JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.items;
+    }
+
+    public static string ToJsonObject<T>(T obj)
+    {
+        return UnityEngine.JsonUtility.ToJson(obj);
+    }
+
+    public static string ToJsonArray<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = array;
+        return UnityEngine.JsonUtility.ToJson(wrapper);
+    }
+
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+}
---
+++ NodeSocketIO_MultiUserTest/Assets/UserObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class UserObject {
+    public string id 		= "";
+    public float x 			= -1;
+	public float y 			= -1;
+	public float z 			= -1;
+	public int r 			= -1;
+	public int g 			= -1;
+	public int b 			= -1;
+	public string message 	= "";
+}
